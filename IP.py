b9acdf9a0a445fe9f0bce5dcccb08a4becaaf8d2
Joan Code <none>
2025-02-08 19:49:15 +0100

    Dark-mode!

--- IP.py
+++ IP.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -10,29 +10,7 @@ def get_ip():
     else:
         ip = request.remote_addr
     
-    html = f'''<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Your IP Address</title>
-        <style>
-            body {{
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                font-size: 50px;
-                font-family: Arial, sans-serif;
-            }}
-        </style>
-    </head>
-    <body>
-        {ip}
-    </body>
-    </html>'''
-    
-    return html
+    return render_template('index.html', ip=ip)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
